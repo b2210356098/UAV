a4d31b435aea7bca616cb1048cae5be941fa7357
Phoneria <90638359+Phoneria@users.noreply.github.com>
2023-03-03 09:46:08 +0300

    Update Deneme.py

--- Deneme.py
+++ Deneme.py
@@ -1,100 +1,34 @@
-from dronekit import Command, connect, VehicleMode, LocationGlobalRelative
-from pymavlink import mavutil
 import time
 import cv2
 import math
-import GPIO
+t_file = open("Thresholds.txt", "a")
 
-
-start_time = time.time()
-t_file = open("Thresholds.txt","w")
-t_file.write("")
-t_file.close()
+from dronekit import connect
+import serial
+import RPi.GPIO as GPIO
 
 
 # Connection
-plane = connect("127.0.0.1:14550", wait_ready=False)
+plane = connect("/dev/ttyACM0", wait_ready=False)
+while (True):
+        if(plane.location.global_frame.lat!=None):
+                break
+print("Armable, armed, version, velocity, alt,lon,lat")
+print(plane.is_armable)
+print(plane.armed)
+print(plane.version)
+print(plane.velocity)
 
-i = 0
-photo_limit = 5
-
-
-x_center = 0  # x location in photograph
-y_center = 0  # y location in photograph
-shapes = [640, 480]
-
-founded_time = 0  # The time, when the target is found
-wait_time = 2  # The time we want to wait for taking second photo
-
-first_x = 0  # x position when photo was taken
-first_y = 0  # y position when photo was taken
-second_x = 0  # x position after wait_time second from first_x
-second_y = 0  # y position after wait_time second from first_y
-
-gps_taken = False  # Checks if second gps is taken or not
-photo_founded = False  # Checks if photo is founded or not
-# Person Recognization Model
-
-classNames = ["person"]
-
-configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-weightPath = "frozen_inference_graph.pb"
-
-net = cv2.dnn_DetectionModel(weightPath, configPath)
-net.setInputSize(320, 320)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-
-
-# Starts Camera
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
-
-# It brings some plane parameters and return real gps coordinates of person
-def gps_calculation(plane_lat, plane_lon, plane_lat2, plane_lon2, photo_x, photo_y, image):
-    # Fotoğraftaki kordinatları alarak tespit edilen cismin uçağa olan gerçek uzaklığını döndüyor
-    def distance_calculation(x, y, shape):
-        yukseklik = 2500
-        yukseklik_alan = 3070.434033764282 * yukseklik ** 2 / 158.5 ** 2  # cm2
-        yukselikteki_birim = math.sqrt(yukseklik_alan / (640 * 480))  # Her bir birim uzunluğun reeldeki cm karşılığı
-
-        # Merkezi orijin olarak alıyor
-        x = x - shape[1] / 2
-        y = -y + shape[0] / 2
-
-        real_x = yukselikteki_birim * x
-        real_y = yukselikteki_birim * y
-
-        return [real_x, real_y]
-
-    plane_dist_x, plane_dist_y = distance_calculation(photo_x, photo_y, image)
-
-    # Uçağın uçtuğu yol ile  coğrafik kuzey noktası arasındaki açıyı buluyor
-    try:
-        angle = math.atan((plane_lat2 - plane_lat) / (plane_lon2 - plane_lon)) * 57.2957795
-    except ZeroDivisionError:
-        angle = 89
-
-    # Hedefin coğrafik kordinatlara göre uçağa olan uzaklığını buluyor
-    real_x = plane_dist_x * math.cos(angle) + plane_dist_y * math.sin(angle)
-    real_y = -plane_dist_x * math.sin(angle) + plane_dist_y * math.cos(angle)
-
-    # Hedefin gps kordinatlarını buluyor
-    gps_x = plane_lat + (180 / math.pi) * (real_y / 6378137)
-    gps_y = plane_lon + (180 / math.pi) * (real_x / 6378137) / math.cos(plane_lat * 57.2957795)
-
-    return [gps_x, gps_y]
-
+print(plane.location.global_frame.alt)
+print(plane.location.global_frame.lat)
+print(plane.location.global_frame.lon)
 
 def servo_control(repeat, sleep, pvm, freq, x1, x2):
 
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pvm, GPIO.OUT)
     p = GPIO.PWM(pvm, freq)
-    p.start(2.5)
+    p.start(50)
     i = 0
     try:
 
@@ -113,111 +47,110 @@ def servo_control(repeat, sleep, pvm, freq, x1, x2):
 def drop_ball(person_gps_x, person_gps_y, current_x, current_y, vel):
 
     m, k, A, g = 0.18, 1, 0.1, 9.8
-    
+
     v_lim = math.sqrt(m * g / (k * A))
-    try:
-        if 15 < plane.location.global_frame.alt<50:
-            ball_drop_time = plane.location.global_frame.alt * 2 / (3 * v_lim)
-        else:
-            ball_drop_time = 30 * 2 / (3 * v_lim)
-    except:
-        ball_drop_time = 30 * 2 / (3 * v_lim)
-
+    ball_drop_time = 30 * 2 / (3 * v_lim)
     drop_distance = vel * ball_drop_time
-    txt= open("Thresholds.txt","a")
-    txt.write("V limit: "+str(v_lim))
-    txt.write(" Ball Drop Time: "+str(ball_drop_time))
-    txt.write(" Drop Distance: "+str(drop_distance))
-    txt.write(" Current_x: "+str(current_x))
-    txt.write(" Current_y: "+str(current_y))
-    txt.write(" Current_alt: "+str(plane.location.global_frame.alt))
-    txt.write(" person_gps_x: "+str(person_gps_x))
-    txt.write(" person_gps_y: "+str(person_gps_y))
-    txt.close()
 
-    if ((current_x - person_gps_x) ** 2 + (current_y - person_gps_y) ** 2) <= drop_distance ** 2:
-        servo_control(1, 0.5, 33, 40, 5, 12.5)
-        servo_control(1, 0.5, 32, 40, 12.5, 5)
+    t_file.write("V limit: " + str(v_lim))
+    t_file.write(" Ball Drop Time: " + str(ball_drop_time))
+    t_file.write(" Drop Distance: " + str(drop_distance))
+    t_file.write(" Current_x: " + str(current_x))
+    t_file.write(" Current_y: " + str(current_y))
+    t_file.write(" Current_alt: " + str(plane.location.global_frame.alt))
+    t_file.write(" person_gps_x: " + str(person_gps_x))
+    t_file.write(" person_gps_y: " + str(person_gps_y))
+    R = 6373.0
 
+    dlon = person_gps_y - current_y
+    dlat = person_gps_x - current_x
+    a = (math.sin(dlat / 2)) ** 2 + math.cos(current_x) * math.cos(person_gps_x) * (math.sin(dlon / 2)) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    print(distance)
 
+    if distance <= drop_distance :
+        servo_control(1,1, 33, 80, 5, 12.5)
+        servo_control(1, 1, 32, 35, 12.5, 5)
+        return True
+    
+    
+flight_time = 10 # minute
+photo_limit = 50
+wait_for_delete = 3 #second
+wait_for_fly = 0.10 #second
 
-
-try:
-    while True:
-        # Person Detection
-        with open("Thresholds.txt","a") as t_file:
-            try:
-                if i >=100:
-                    break
-                # If person not detected
-                if photo_founded:
-                    if not gps_taken:
-                        # if time difference is bigger than wait_time, it takes second coordinates
-
-                        if time.time() - founded_time >= wait_time:
-                            second_x = plane.location.global_frame.lat
-                            second_y = plane.location.global_frame.lon
-                            gps_taken = True
-                    else:
-                        velocity_of_plane = math.sqrt((first_x - second_x) ** 2 + (first_y - second_y) ** 2) / wait_time
-
-                        person_gps_x = gps_calculation(first_x, first_y, second_x, second_y, shapes)[0]
-                        person_gps_y = gps_calculation(first_x, first_y, second_x, second_y, shapes)[1]
-                        t_file.write("Velocity : " + velocity_of_plane)
+time.sleep(wait_for_fly)
 
 
 
-                        drop_ball(person_gps_x, person_gps_y, plane.location.global_frame.lat, plane.location.global_frame.lon, velocity_of_plane)
-
-                # If person not detected, it searches for person and takes its photo and coordinates
-
-                success, img = cap.read()
-                shapes = img.shape
-
-                classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-
-                if len(classIds) != 0:
-
-                    i += 1
-
-                    if i >= photo_limit and not photo_founded:
-                        photo_founded = True
-                        cv2.imwrite("first_person.png", img)
-                        founded_time = time.time()
-                        t_file.write("TIME : " +str(founded_time-start_time)+"\n\n\n\n\n\n\n\n")
-                        first_x = plane.location.global_frame.lat
-                        first_y = plane.location.global_frame.lon
-
-                    for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                        x_center = (box[0] + box[2]) / 2
-                        y_center = (box[1] + box[3]) / 2
-                        img = cv2.rectangle(img, box, (0, 255, 0), thickness=2)
-                        img = cv2.putText(img, classNames[classId - 1].upper() + str(confs), (box[0], box[1]),
-                                          cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 2)
-
-
-                    t_file.write(str(i) + "-> Person Founded " + str(confs) + "\n")
-                    cv2.imwrite(str(i)+"_"+str(confs) + "_person.png", img)
-                    #print(str(i) + "-> Person Founded " + str(confs) + "\n")
-
-            except:
-                pass
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
 
 
+classNames=["person"]
+
+configPath ="ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightPath ="frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)
 
 
 
-except Exception as e:
-    t_file = open("Thresholds.txt","a")
-    t_file.write("ERORRR :"+e)
-    t_file.close()
+i=0
+last_time = time.time()
+start_time = time.time()
+first_x = 0
+first_y = 0
+photo_founded = False
+while (time.time()-start_time<flight_time*60):
+    try:
+        if not photo_founded :
+            success,img = cap.read()
 
-finally:
-    t_file = open("Thresholds.txt","a")
-    finish_time = time.time()-start_time
-    t_file.write("Finish Time :" +str(finish_time)+"\n\n\n\n\n\n\n\n\n")
-    t_file.close()
+            classIds,confs,bbox=net.detect(img,confThreshold = 0.5)
+
+
+            if time.time()-last_time >= wait_for_delete:
+                i = 0
+            if len(classIds)!=0:
+                last_time = time.time()
+
+                for classId,confidence ,box in zip(classIds.flatten(),confs.flatten(),bbox):
+                    img = cv2.rectangle(img, box, (0, 255, 0), thickness=2)
+                    img = cv2.putText(img, classNames[classId - 1].upper()+str(confs), (box[0], box[1]), cv2.FONT_HERSHEY_COMPLEX, 2,(0, 255, 0), 2)
+
+                i += 1
+                t_file.write(str(i)+ "-> Person Founded "+str(confs)+"\n")
+                cv2.imwrite(str(i)+".png",img)
+                print(str(i)+ "-> Person Founded "+str(confs))
+
+
+                if i>=photo_limit:
+                    first_x = plane.location.global_frame.lat
+                    first_y = plane.location.global_frame.lon
+                    t_file.write("Person GPS : " + str(first_x) +" "+ str(first_y)+ "\n")
+
+                    cv2.imwrite("mission.png",img)
+                    photo_founded = True
 
 
 
+            #cv2.imshow("Output",img)
+            #cv2.waitKey(1)
+        else:
+
+            drop_ball(first_x,first_y,plane.location.global_frame.lat,plane.location.global_frame.lon,20)
+            if drop_ball(first_x,first_y,plane.location.global_frame.lat,plane.location.global_frame.lon,20):
+                break
+
+    except Exception as e:
+        print(e)
+
+t_file.close()
