91938ec8d33fbeae0b7dbd116982e126071a5595
Phoneria <90638359+Phoneria@users.noreply.github.com>
2023-03-08 17:43:38 +0300

    Create ImageSend.py

---
+++ ImageSend.py
@@ -0,0 +1,31 @@
+import socket
+import cv2
+import time
+from PIL import Image
+
+def compress(image_file):
+    image = Image.open(image_file)
+    image.save("frame_2.jpeg", "jpeg", optimize = True, quality = 10)
+    return
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(("", 5297)) #8888
+server.listen(5)
+cap = cv2.VideoCapture(0)
+cap.set(3,320) # width is id no 3
+cap.set(4,240) # height is id no 4
+cap.set(10,100) # Brightness id is 10
+
+clientsocket, address = server.accept()
+
+while True:
+    success, img = cap.read()
+    cv2.imwrite("frame.jpeg", img)
+    compress("frame.jpeg")
+    with open("frame_2.jpeg","rb") as image:
+        read_image = image.read()
+        byteimg = bytearray(read_image)
+        print("CONVERT TO BYTE")
+    clientsocket.send(byteimg)
+    print("SEND") 
+    time.sleep(0.5)
