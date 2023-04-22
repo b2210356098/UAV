6d47dfd124b39153bf2c880390787e56da15f08f
b2210356098 <115803471+b2210356098@users.noreply.github.com>
2023-04-22 14:18:04 +0300

    Update ImageSend.py

--- ImageSend.py
+++ ImageSend.py
@@ -1,31 +1,33 @@
 import socket
-import cv2
 import time
-from PIL import Image
+import cv2
 
-def compress(image_file):
-    image = Image.open(image_file)
-    image.save("frame_2.jpeg", "jpeg", optimize = True, quality = 10)
-    return
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("", 9999)) #8888 9999 5297
+#print("SERVER")
+server.bind(("", 5297))
+#print("BIND")
 server.listen(5)
+#print("LISTEN")
 cap = cv2.VideoCapture(0)
+"""
 cap.set(3,320) # width is id no 3
 cap.set(4,240) # height is id no 4
 cap.set(10,100) # Brightness id is 10
-
+"""
+#print("VIDEO CAPTURE")
 clientsocket, address = server.accept()
+#print("BAGLAN")
 
 while True:
     success, img = cap.read()
-    cv2.imwrite("frame.jpeg", img)
-    compress("frame.jpeg")
-    with open("frame_2.jpeg","rb") as image:
+    #print("VIDEO READ")
+    cv2.imwrite("frame.jpeg", img, [cv2.IMWRITE_JPEG_QUALITY, 50])
+    #print("WRITE FRAME")
+    with open("frame.jpeg","rb") as image:
         read_image = image.read()
+        #print("READ IMAGE")
         byteimg = bytearray(read_image)
-        print("CONVERT TO BYTE")
+        #print("CONVERT TO BYTE")
     clientsocket.send(byteimg)
-    print("SEND") 
-    time.sleep(0.5)
+    #print("SEND")
